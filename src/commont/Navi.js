3be1de834c6a4cd5a6e608613bd911384e158872
saelin5658 <127266007+saelin5658@users.noreply.github.com>
2023-04-13 10:45:24 +0900

    href

--- src/commont/Navi.js
+++ src/commont/Navi.js
@@ -35,7 +35,7 @@ function Navi() {
   return (
     <nav className={`nav ${show && "nav__black"}`}>
       <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png' 
-      alt='NETFLIX logo' className='nav__logo' onClick={()=>{window.location.href = ""}}/>
+      alt='NETFLIX logo' className='nav__logo' onClick={()=>{window.location.href = "https://saelin5658.github.io/movienet_app/"}}/>
       <input type='search' placeholder='영화이름입력' className='nav__input' onChange={onChange} value={searchValue}/>
       <img src='https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41'
        alt='user logo' className='nav__avatar'/>
