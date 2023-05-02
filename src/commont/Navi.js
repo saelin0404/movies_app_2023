ac3a048f86deb0748b67471d1282e21fb0535d88
saelin0440 <131337654+saelin0404@users.noreply.github.com>
2023-05-02 09:31:38 +0900

    edit

--- src/commont/Navi.js
+++ src/commont/Navi.js
@@ -34,8 +34,6 @@ function Navi() {
   //onClick={window.location.reload} 누를때마다 로드
   return (
     <nav className={`nav ${show && "nav__black"}`}>
-      <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png' 
-      alt='NETFLIX logo' className='nav__logo' />
       <input type='search' placeholder='영화이름입력' className='nav__input' onChange={onChange} value={searchValue}/>
       <Link to={'myprofile'}>
       <img src='https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41'
